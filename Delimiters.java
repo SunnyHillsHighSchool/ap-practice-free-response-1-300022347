ddcf41802da491beb3d4aadf6dba40f6f9e06d7f
Kyle Kim <>
2021-04-17 06:23:04 +0000

    Completed free response practice

--- Delimiters.java
+++ Delimiters.java
@@ -1,4 +1,9 @@
 import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+
+//Programmer: Kyle Kim
+//Period: 2
 
 public class Delimiters
 {
@@ -11,13 +16,38 @@ public class Delimiters
 		closeDel = close;
 	}
 
-	public ArrayList<String> getDelimitersList(String[] tokens)
-	{
-		return null;
-	}
-
 	public boolean isBalanced(ArrayList<String> delimiters)
 	{ 
 		return false;
 	}
+
+
+  public ArrayList<String> getDelimitersList(String[] tokens)
+  {
+      //create an arraylist for the delimiters
+      ArrayList<String> newDelimiter = new ArrayList<>();
+      //change the list of tokens into an arraylist
+      List<String> tokensA= new ArrayList<String>(Arrays.asList(tokens));
+      //loop through the arraylist tokens
+      for(int c=0; c<tokensA.size(); c++)
+      {
+        //if tokens is equal to the open delimiter add it to newdelimiter list
+        if((tokensA.get(c)).equals(openDel))
+        {
+          String current=tokensA.get(c);
+          newDelimiter.add(current);
+        }
+        //if tokens is equal to the close delimiter add it to newdelimiter list
+        else if((tokensA.get(c)).equals(closeDel))
+        {
+          String current=tokensA.get(c);
+          newDelimiter.add(current);
+        }
+        else
+        {
+        }
+      }
+      //return newDelimiter list
+      return newDelimiter;
+  }
 }
